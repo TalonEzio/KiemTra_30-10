af14d98946241503190cb3275b99c3e40214910c
TalonEzio <vuthemanh1707@gmail.com>
2023-10-31 08:41:20 +0700

    Add project files.

---
+++ Helpers/CipherHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra_30_10.Helpers
+{
+    public class CipherHelper
+    {
+        public static string Encrypt<T>(string data, string key, string initVector = "abcede0123456789")
+        where T : SymmetricAlgorithm
+
+        {
+            string algorithmName = typeof(T).Name;
+
+            using (var algorithm = SymmetricAlgorithm.Create(algorithmName) as T)
+            {
+                var ivLength = algorithm.BlockSize / 8;
+                byte[] initializationVector = Encoding.ASCII.GetBytes(initVector, 0, ivLength);
+
+                //Console.WriteLine($"Key size: {algorithm.KeySize}, Block size: {algorithm.BlockSize}, Feedback size: {algorithm.FeedbackSize}");
+
+                int keyLength = 16;
+                if (algorithm is DES)
+                    keyLength = 8;
+                if (algorithm is TripleDES)
+                    keyLength = 24;
+                var keyByte = Encoding.UTF8.GetBytes(key, 0, keyLength);
+
+
+                algorithm.Key = keyByte;
+                algorithm.IV = initializationVector;
+                var symmetricEncryptor = algorithm.CreateEncryptor(algorithm.Key, algorithm.IV);
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream as Stream,
+                               symmetricEncryptor, CryptoStreamMode.Write))
+                    {
+                        using (var streamWriter = new StreamWriter(cryptoStream as Stream))
+                        {
+                            streamWriter.Write(data);
+                        }
+                        return Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+        public static string Decrypt<T>(string cipherText, string key, string initVector = "abcede0123456789")
+            where T : SymmetricAlgorithm
+        {
+
+            string algorithmName = typeof(T).Name;
+
+            byte[] buffer = Convert.FromBase64String(cipherText);
+            using (T algorithm = SymmetricAlgorithm.Create(algorithmName) as T)
+            {
+                var ivLength = algorithm.BlockSize / 8;
+                byte[] initializationVector = Encoding.ASCII.GetBytes(initVector, 0, ivLength);
+
+                //Console.WriteLine($"Key size: {algorithm.KeySize}, Block size: {algorithm.BlockSize}, Feedback size: {algorithm.FeedbackSize}");
+
+                int keyLength = 16;
+                if (algorithm is DES)
+                    keyLength = 8;
+                if (algorithm is TripleDES)
+                    keyLength = 24;
+                var keyByte = Encoding.UTF8.GetBytes(key, 0, keyLength);
+
+
+                algorithm.Key = keyByte;
+                algorithm.IV = initializationVector;
+                var decryptor = algorithm.CreateDecryptor(algorithm.Key, algorithm.IV);
+                using (var memoryStream = new MemoryStream(buffer))
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream as Stream,
+                               decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var streamReader = new StreamReader(cryptoStream as Stream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using KiemTra_30_10.Helpers;
+
+namespace KiemTra_30_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Console.OutputEncoding = Encoding.Unicode;
+            string key = GenerateKeyForAll();
+            Console.Write("Chuỗi cần mã hóa: ");
+            string input = Console.ReadLine() ?? "";
+            string aesEncrypt = CipherHelper.Encrypt<Aes>(input, key);
+            string rc2Encrypt = CipherHelper.Encrypt<RC2>(input, key);
+            string rijndaelEncrypt = CipherHelper.Encrypt<Rijndael>(input, key);
+            string desEncrypt = CipherHelper.Encrypt<DES>(input, key);
+            string tripleDesEncrypt = CipherHelper.Encrypt<TripleDES>(input, key);
+
+
+
+            Console.WriteLine($"Mã hóa theo aes: {aesEncrypt}");
+            Console.WriteLine($"Mã hóa theo RC2: {rc2Encrypt}");
+            Console.WriteLine($"Mã hóa theo Rijndael: {rijndaelEncrypt}");// giống aes
+            Console.WriteLine($"Mã hóa theo DES: {desEncrypt}");
+            Console.WriteLine($"Mã hóa theo TripleDES: {tripleDesEncrypt}");
+
+            string aesDecrypt = CipherHelper.Decrypt<Aes>(aesEncrypt, key);
+            string rc2Decrypt = CipherHelper.Decrypt<RC2>(rc2Encrypt, key);
+            string rijndaelDecrypt = CipherHelper.Decrypt<Rijndael>(rijndaelEncrypt, key);
+            string desDecrypt = CipherHelper.Decrypt<DES>(desEncrypt, key);
+            string tripleDesDecrypt = CipherHelper.Decrypt<TripleDES>(tripleDesEncrypt, key);
+
+            Console.WriteLine("".PadLeft(50,'*'));
+
+            Console.WriteLine($"Giải mã theo aes: {aesDecrypt}");
+            Console.WriteLine($"Giải mã theo RC2: {rc2Decrypt}");
+            Console.WriteLine($"Giải mã theo Rijndael: {rijndaelDecrypt}");// giống aes
+            Console.WriteLine($"Giải mã theo DES: {desDecrypt}");
+            Console.WriteLine($"Giải mã theo TripleDES: {tripleDesDecrypt}");
+
+
+
+            Console.ReadLine();
+        }
+
+
+        public static string GenerateKeyForAll()
+        {
+
+            var tripleDes = new TripleDESCryptoServiceProvider();
+
+            tripleDes.GenerateKey();
+
+            byte[] key = tripleDes.Key;
+
+            return Encoding.ASCII.GetString(key);
+
+        }
+    }
+}
